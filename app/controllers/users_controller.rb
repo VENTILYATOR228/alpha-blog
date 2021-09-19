5df9b8cea1a1718b1b4c0d6782b46e1dd8be3f41
VENTILYATOR228 <pobeditel.com@gmail.com>
2021-09-19 12:09:52 +0300

    Add links on pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Profile was updated successfully!"
-      redirect_to articles_path
+      redirect_to users_path(@user)
     else
       render 'edit'
     end
